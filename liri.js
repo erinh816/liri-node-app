d6df1a4bb340645bc846c0d323eaf69a71a40cb7
Silu Hu <silu@Silus-MacBook-Pro.local>
2018-08-23 16:03:40 -0400

    add

--- liri.js
+++ liri.js
@@ -1,5 +1,19 @@
 require("dotenv").config();
+var keys = require('./keys.js')
+
+var MOVIE = require("./omdb");
+
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-var reddit = new reddit(keys.reddit);
+// var client = new Twitter(keys.twitter);
+var tumblr = new Tumblr(keys.tumblr);
+
+
+var movie = new Movie();
+
+var search = search.argv[2];
+
+var term = process.argv.slice(3).join("");
+
+
+
---
+++ omdb.js
@@ -0,0 +1,33 @@
+var request = require ("request");
+var fs = require ("fs");
+
+var omdb = function(){
+    var divider = "\n------------------------------------------------------------\n\n";
+    this.findMovie = function(movie){
+        var URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        request(URL, function(err, response, body){
+            console.log(response);
+
+            var jsonData = JSON.parse(body);
+
+            var movieData = [
+                "Movie:"+ jsonData.Title + "\n" +
+                "Year:" + jsonData.Year + "\n" +
+                "IMDB Rating:" + jsonData.imdbRating + "\n" +
+             //    "Rotten Tomatoes Rating:" + jsonData.Title + "\n" +
+                "Country:" + jsonData.Country + "\n" +
+                "Language:" + jsonData.Language + "\n" +
+                "Plot:" + jsonData.Plot + "\n" +
+                "Actors:" + jsonData.Actors + "\n"
+            ]
+
+            fs.appendFile("log.txt", movieData + divider, function(err){
+                if (err) throw err;
+                console.log(movieData);
+            });
+        });
+    };
+};
+
+module.exports = MOVIE;
+ 
